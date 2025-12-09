5681cb5a2168ad298c6e77cfdc90024125560b47
WoutHofstra <hofstrawout@gmail.com>
2025-12-09 18:09:58 +0200

    Added serviceBusClient creation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using System.Security.AccessControl;
 using asyncMessageProcessor;
+using Config;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Messaging.ServiceBus;
 
 Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
+        var sbSettings = new ServiceBusSettings();
+        context.Configuration.GetSection("serviceBus").Bind(sbSettings);
+
+        services.AddSingleton(sbSettings);
+
+        services.AddSingleton<ServiceBusClient>(_ =>
+            new ServiceBusClient(sbSettings.ConnectionString));
+
         services.AddHostedService<Worker>();
     })
     .Build()
